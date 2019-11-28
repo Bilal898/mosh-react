f5c747755300e8e0f83e8e1b242812a8af32ac2c
bilal898 <bfhamid@gmail.com>
2019-11-28 11:31:07 +0500

    lesson 87. sort icon

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -16,13 +16,25 @@ export default class TableHeader extends Component {
         
     }
     
+    renderSortIcon = column => {
+        const { sortColumn} = this.props
+
+        if (column.path !== sortColumn.path) return null
+
+        if (sortColumn.order === 'asc')
+            return <i className="fa fa-sort-asc"></i>
+
+        return <i className="fa fa-sort-desc"></i>
+    }
     render() {
         return (
             <thead>
                 <tr>
                     {this.props.columns.map(column => (
                         <th key={column.path || column.key}
-                        onClick={() => this.raiseSort(column.path)}>{column.label}</th>
+                        className="clickable"
+                        onClick={() => this.raiseSort(column.path)}>
+                        {column.label} {this.renderSortIcon(column)}</th>
                     ))}
                 </tr>
             </thead>
